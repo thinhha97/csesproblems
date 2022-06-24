4e44079acde5e6fdfdbd5c5e1931042b28db27e7
Thinh Ha <havanthinh97@gmail.com>
2022-06-24 13:52:41 +0700

    Initial commit.

---
+++ 1weirdalgorithm.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+int main() {
+	ll n;
+	std::cin>>n;
+	std::cout<<n;
+	while (n > 1) {
+		if (n%2 == 0) {
+			n /= 2;
+		} else {
+			n = n*3+1;
+		}
+		std::cout<<" "<<n;
+	}
+}
